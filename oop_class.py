adc6a33474dad0c731c8f3402317cf2ca2f1ba76
LeTrungPham <letrungpham700@github.com>
2022-08-11 09:35:27 +0700

    add

---
+++ oop_class.py
@@ -0,0 +1,21 @@
+
+class Nhanvien:
+    def __init__(self,name,age,address):
+        self.name=name
+        self.age=age
+        self.address=address
+
+class InNVM(Nhanvien):
+    def __init__(self):
+        self.name = input("Input name: ")
+        self.age = int(input("Input age: "))
+        self.address = input("Input address: ")
+
+class OutNV(InNVM):
+    def outnv(self):
+        print("Ten: "+self.name,"\tTuoi: "+str(self.age),"\tAddress: "+self.address)
+
+if __name__ == '__main__':
+    nv = OutNV()
+    nv.outnv()
+
